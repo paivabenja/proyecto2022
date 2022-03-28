a36dc7b0ffabc832f6252df13208bc8c0b162a01
paivabenja <paivabenja05@gmail.com>
2022-03-28 10:22:35 -0300

    navbar

--- app-peliculas/src/App.js
+++ app-peliculas/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Navbar from "./Navbar";
+import NavbarComp from "./components/NavbarComp";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <NavbarComp/>
     </div>
   );
 }
