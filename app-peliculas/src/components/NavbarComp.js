bf8413242e4d402c694ac429585620f77972664b
paivabenja <paivabenja05@gmail.com>
2022-04-04 10:28:33 -0300

    navbar terminada

--- app-peliculas/src/components/NavbarComp.js
+++ app-peliculas/src/components/NavbarComp.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
-import { Navbar,Nav,NavDropdown,Container} from "react-bootstrap";
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Container,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 
 export default class NavbarComp extends Component {
   render() {
@@ -7,31 +15,29 @@ export default class NavbarComp extends Component {
       <div>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
           <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="#home">Virgenlix</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    Something
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
+                <Nav.Link href="#nuevo">Nuevo</Nav.Link>
+                <Nav.Link href="#tendencia">Tendencia</Nav.Link>
+                <Nav.Link href="#clasicos">Clasicos</Nav.Link>
+                <NavDropdown title="Programas">
+                  <NavDropdown.Item href="#series">Series</NavDropdown.Item>
+                  <NavDropdown.Item href="#Peliculas">
+                    Peliculas
                   </NavDropdown.Item>
                 </NavDropdown>
               </Nav>
               <Nav>
-                <Nav.Link href="#deets">More deets</Nav.Link>
-                <Nav.Link eventKey={2} href="#memes">
-                  Dank memes
-                </Nav.Link>
+                <Form className="d-flex">
+                  <FormControl
+                    type="text"
+                    placeholder="Search"
+                    className="me-2"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
               </Nav>
             </Navbar.Collapse>
           </Container>
