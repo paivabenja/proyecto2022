46875a35adbc1764d5061fa842d3992554bd8b25
santiagobernert <s.bernert@alumno.etec.um.edu.ar>
2022-05-09 09:58:40 -0300

    9/5

---
+++ backend/server/peliculas/pelicula.py
@@ -0,0 +1,11 @@
+
+
+class Media:
+    __attrs__ = [
+        'poster', 'casting', 'director', 'resumen', 'año', 'categoria', 'rating',
+        'id', 
+    ]
+    def __init__(self, titulo):
+        self.titulo = titulo
+
+    
