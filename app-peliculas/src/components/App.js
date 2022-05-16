908765cfa719e43f377e0142841783aef7c7d2a1
paivabenja <paivabenja05@gmail.com>
2022-05-16 08:16:57 -0300

    arreglado para la presentacion

--- app-peliculas/src/components/App.js
+++ app-peliculas/src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import NavbarComp from "./NavbarComp";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import SignUp from "./SignUp";
 import Home from "./Home";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,29 +8,32 @@ import "../styles/App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <NavbarComp
-          nombre="nombre"
-          boton1="boton1"
-          boton2="boton2"
-          boton3="boton3"
-          boton4="boton4"
-          boton5="boton5"
-          boton6="boton6"
-          boton7="boton7"
-          boton8="boton8"
-        />
-        <Switch>
-          <Route path="/">
-            <Home></Home>
-          </Route>
-          <Route path="/signup">
-            <SignUp></SignUp>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    // <Router>
+    <div className="App">
+      <NavbarComp
+        nombre="nombre"
+        boton1="boton1"
+        boton2="boton2"
+        boton3="boton3"
+        boton4="boton4"
+        boton5="boton5"
+        boton6="boton6"
+        boton7="boton7"
+        boton8="boton8"
+      />
+      {/* <Switch> */}
+      {/* <Route path="/"> */}
+      <Home></Home>
+      {/* </Route> */}
+      {/* <Route path="/signup"> */}
+
+      {/*signup */}
+      {/* <SignUp></SignUp> */}
+
+      {/* </Route> */}
+      {/* </Switch> */}
+    </div>
+    // </Router>
   );
 }
 
