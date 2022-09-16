424e7ab3b3e12161c99464ff622a3c1cd0a89379
paivabenja <paivabenja05@gmail.com>
2022-09-16 09:27:54 -0300

    added signup and login page

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,23 +1,44 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import '../styles/LogIn.css';
+import React, { useState, useEffect } from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 export function SignUp() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log('que pasa chavales');
+  const [user, setUser] = useState({ username: '', password: '' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+    if (e.target[0].value != '' && e.target[1].value != '') {
+      setUser({ username: e.target[0].value, password: e.target[1].value });
+      alert('usuario: ' + e.target[0].value + '\ncontrasenia: ' + e.target[1].value);
+    } else if (e.target[0].value == '' && e.target[1].value == '') {
+      alert('llena las dos cosas gordo puto');
+    } else if (e.target[0].value == '') {
+      alert('pone usuario papi');
+    } else if (e.target[1].value == '') {
+      alert('pone contrasenia papi');
+    }
   };
 
   return (
-    <div>
-      <form
-        onSubmit={(e) => {
-          handleSubmit(e);
-        }}
-      >
-        <input type="text" placeholder='Username'/>
-        <input type="password" placeholder='Password'/>
-        <input type="submit" placeholder="hola" />
-      </form>
+    <div className="login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="Username">
+          <Form.Label>Sign in to an existing account</Form.Label>
+          <Form.Control placeholder="Enter Username" />
+          <Form.Text className="text-muted">
+            If you forgot your username go fuck yourself
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button style={{ justifySelf: 'center' }} type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
