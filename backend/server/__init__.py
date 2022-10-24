f9dbc0d0abf2127cd135232f9a9662bb98472755
santiagobernert <s.bernert@alumno.etec.um.edu.ar>
2022-10-24 09:59:45 -0300

    created token route

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_jwt_extended import JWTManager
 from os import path
 
 db = SQLAlchemy()
@@ -11,6 +12,8 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///../../backend/server/{DB_NAME}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.secret_key = 'keloke'
+    app.config["JWT_SECRET_KEY"] = "koinoyokan"
+    jwt = JWTManager(app)
     db.init_app(app)
 
     from .login import login
--- backend/server/login.py
+++ backend/server/login.py
@@ -1,72 +1,82 @@
-from flask import Flask, request, redirect, url_for, Blueprint, render_template, flash, jsonify
+from flask import request, Blueprint, render_template, jsonify
 from flask_login import login_user, login_required, logout_user, current_user
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 from .users import User, new_user
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 login = Blueprint('login', __name__)
 
+
+@login.route('/token', methods=['POST'])
+def create_token():
+    username = request.json['username']
+    email = request.json['email']
+    password = request.json['password']
+    if email != 'test' and password != 'test':
+        return jsonify({'error': 'bad username or password'}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+
 @login.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        usuario = request.json['username']
+        username = request.json['username']
         email = request.json['email']
-        contraseña = request.json['contraseña']
-        contraseña2 = request.json['contraseña2']
+        password = request.json['password']
 
-        us_existe = User.query.filter_by(nombre=usuario).first()
+        us_existe = User.query.filter_by(nombre=username).first()
         email_existe = User.query.filter_by(nombre=email).first()
 
         if us_existe or email_existe:
-            print('usuario ya existe')
-            return('usuario ya existe')
+            print('username already exists')
+            return jsonify({'error': 'username already exists'}), 409
         elif email_existe:
-            print('email ya usado')
-            return('email ya usado')
-        elif contraseña != contraseña2:
-            return('las constraseñas no coinciden')
-        elif contraseña == '':
-                return('contraseña incompleta')
-        elif len(contraseña) < 6:
-            print('contraseña muy corta')
-            return('contraseña muy corta')
-        elif len(usuario) < 3:
-            return('nombre de usuario muy corto')
+            print('email already used')
+            return jsonify({'error': 'email already used'}), 409
+        elif password == '':
+                return('password incomplete')
+        elif len(password) < 6:
+            print('password too short')
+            return('password too short')
+        elif len(username) < 3:
+            return('username too short')
         else:
-            nu = new_user(usuario, email, generate_password_hash(contraseña, method='sha256'))
-            print('usuario creado')
+            nu = new_user(username, email, generate_password_hash(password, method='sha256'))
+            print('username creado')
             login_user(nu, remember=True)
-            response = jsonify({'usuario': nu.__asdict__()})
+            response = jsonify({'username': nu.__asdict__()})
             response.headers.add('Access-Control-Allow-Origin', '*')
-            return 
+            return response
 
-    return render_template('index.html', pagina='signup', error='no se encontro usuario', usuario='')
 
 @login.route('/login', methods=['GET', 'POST'])
 def log_in():
     if request.method == 'POST':
         email = request.json['email']
-        contraseña = request.json['contraseña']
+        password = request.json['password']
 
-        usuario = User.query.filter_by(email=email).first()
-        if usuario:
-            if check_password_hash(usuario.contraseña, contraseña):
+        username = User.query.filter_by(email=email).first()
+        
+        if username:
+            if check_password_hash(username.password, password):
                 print('Logged in successfully!')
-                flash('Logged in successfully!')
-                login_user(usuario, remember=True)
-                response = jsonify({'usuario': usuario.__asdict__()})
+                login_user(username, remember=True)
+                response = jsonify({'username': username.__asdict__()})
                 response.headers.add('Access-Control-Allow-Origin', '*')
                 return response
             else:
-                print('Incorrect password, try again.')
-                return('Incorrect password, try again.')
+                print('password incorrecta')
+                return jsonify({'error': 'incorrect password'}), 401
         else:
-            print('Email does not exist.')
-            return('Email does not exist.')
+            print('username no existe')
+            return jsonify({'error': 'username does not exist'}), 401
 
-    return {'usuario': 'no logueado'}
+    return jsonify({'username': 'not logged'})
 
 @login_required
 @login.route('/logout')
 def logout():
     logout_user(current_user)
-    return {'usuario': 'no logueado'}
+    return {'username': 'not logged'}
